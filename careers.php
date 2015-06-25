eddee4634f56f10147e35fa2080c73d9ef596c41
Praveen Raj <praveenraj@uniqpix.in>
2015-06-25 11:11:31 +0530

    iw-web code

---
+++ careers.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Indian Westeros | Careers</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php include('link.html'); ?>
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+   
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+        
+        
+        
+        <div class="container-fluid career-index">
+            <?php include('nav.html'); ?>
+        <div class="table"><div class="text-center cell"><div class="intro-text font">CAREERS</div>
+            
+        </div></div>
+             </div>
+        <div class="container-fluid creative-council-wrap text-center">
+        <div class="row ">
+            <div class="col-md-8 col-sm-8" >
+                <h3>Vacancies</h3>
+                <hr style="margin-top: 20px;margin-bottom: 10px;" class="red">
+            <div class="container text-left pd-left">
+                <h3 class="bold">Senior Graphic Designers</h3>
+                <h4>Online Content</h4>
+               <hr class="text-left" style="margin:0 ;"><br>
+                <p>Do you have...</p>
+                <p>&#8226; 3+ years of experience in creating Graphic content</p>
+                 <p>&#8226; experience in web banners and flash</p>
+                 <p>&#8226; impressive skills in image editing and visual effects</p>
+                 <p>&#8226; experience working against timelines</p>
+                 <p>&#8226; expertise in Photoshop, Illustrator, HTML, CSS</p>
+                 <p>&#8226; a B.Sc, VisCom, BCA, MCA, M.Sc degree</p>
+                 <p>&#8226;a positive attitude</p><br>
+                <a href=""><p style="color: #ec1c24">Apply to info@indianwesteros.com</p></a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 council-bg"></div>
+            </div>
+        </div>
+        
+        
+        <div class="container-fluid creative-council-wrap text-center">
+        <div class="row ">
+             <div class="col-md-4 col-sm-4 council-bg"></div>
+            <div class="col-md-8 col-sm-8" >
+                <h3>Vacancies</h3>
+                <hr style="margin-top: 20px;margin-bottom: 10px;" class="red">
+            <div class="container text-left pd-left">
+                <h3 class="bold">Senior Graphic Designers</h3>
+                <h4>Online Content</h4>
+               <hr class="text-left" style="margin:0 ;"><br>
+                <p>Do you have...</p>
+                <p>&#8226; 3+ years of experience in creating Graphic content</p>
+                 <p>&#8226; experience in web banners and flash</p>
+                 <p>&#8226; impressive skills in image editing and visual effects</p>
+                 <p>&#8226; experience working against timelines</p>
+                 <p>&#8226; expertise in Photoshop, Illustrator, HTML, CSS</p>
+                 <p>&#8226; a B.Sc, VisCom, BCA, MCA, M.Sc degree</p>
+                 <p>&#8226; a positive attitude</p><br>
+                <a href=""><p style="color: #ec1c24">Apply to info@indianwesteros.com</p></a>
+                </div>
+            </div>
+           
+            </div>
+        </div>
+        
+       
+   
+       
+        
+<?php include('footer.html'); ?>
+   
+ <!-- /container --><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Indian Westeros</title>
+        <meta name="description" content="">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <?php include('link.html'); ?>
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+       <div id="sb-site">
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+   
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+   <div class="container-fluid index">
+    <video loop autoplay>
+        <source src="img/main-bg.ogv" type="video/ogv">
+            <source src="img/main-bg.webm" type="video/webm">
+    </video>
+
+    <div class="black-mask">
+        <?php include( 'nav.html'); ?>
+        <div class="table">
+            <div class="text-center cell">
+                <div class="intro-text">WE ARE HERE TO
+                    <br><span>CREATE &amp; TRANSFORM BRANDS</span>
+                    <br>THAT LASTS FOREVER </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+<!--      section 1  -->
+    
+<div class="container-fluid wrap01 text-center">
+    <div class="container">
+        <div class="max-cont">
+           <div class="wrap01Text"> We help brands to solve their business challenges,<br> with intellectual ideas that revolutionize the<br> competitive landscape.</div>
+            <div class="row list-wrap">
+                <div class="col-md-4">
+               
+                    <div class="leftSpan">
+                    <span class="glyphicon glyphicon-bullhorn"></span></div>
+                    <div class="rightSpan"><a>Advertising</a></div>
+                
+                
+                </div>
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-stats"></span></div>
+                    <div class="rightSpan"><a>Strategic Planning</a></div>
+                
+                
+                </div>
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-edit"></span></div>
+                    <div class="rightSpan"><a>Creative Design</a></div>
+              
+                
+                </div>
+            </div>
+            <div class="row list-wrap">
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-thumbs-up"></span></div>
+                    <div class="rightSpan"><a>Digital Marketing</a></div>
+                
+                
+                </div>
+                <div class="col-md-4">
+               
+                    <div class="leftSpan"><span class="glyphicon glyphicon-random"></span></div>
+                    <div class="rightSpan"><a>Experiential Marketing</a></div>
+               
+                
+                </div>
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-facetime-video"></span></div>
+                    <div class="rightSpan"><a>Media Relations</a></div>
+                </div>
+                
+                
+            </div>
+            <div class="row list-wrap">
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-user"></span></div>
+                    <div class="rightSpan"><a>Public Affairs</a></div>
+               
+                
+                </div>
+                <div class="col-md-4">
+               
+                    <div class="leftSpan"><span class="glyphicon glyphicon-heart"></span></div>
+                    <div class="rightSpan"><a>Public Relations</a></div>
+               
+                
+                </div>
+                <div class="col-md-4">
+                
+                    <div class="leftSpan"><span class="glyphicon glyphicon-signal"></span></div>
+                    <div class="rightSpan"><a>Trade Marketing</a></div>
+                
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+       
+        <div class="container wrap02">
+          <div class="row">
+              <div class="col-md-3 text-center"><img src="img/vccircle.jpg"/></div>
+              <div class="col-md-3 text-center"><img src="img/yamaha.jpg"/></div>
+              <div class="col-md-3 text-center"><img src="img/espa.jpg"/></div>
+              <div class="col-md-3 text-center"><img src="img/vccircle.jpg"/></div>
+          </div>
+           </div>
+           
+           
+           <div class="container-fluid wrap03">
+              <div class="row-fluid">
+                  <div class="col-md-8 big-coloumn">
+                  <div class="table">
+                      <div class="cell text-center big-text">Neque porro quisquam est qui <br>dolorem ipsum
+                      <br> <br>
+                      <button class="btn-default">View Project</button>
+                      </div>
+                      
+                  </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-12 small-coloumn01 ">
+                         <img src="img/zoom.png" alt="" class="zoom-icon"/>
+                          <div class="content-box">
+                           
+                            <h5>Neque <span>porro</span></h5>
+                        <p>Neque porro quisquam est qui <br>dolorem ipsum</p>
+                        </div>
+                        </div>
+                        <div class="col-md-12 small-coloumn02"></div>
+                    </div>
+                </div>
+              </div>
+               
+           </div>
+           <div class="container-fluid wrap04 text-center">
+              <p class="quote">In marketing to create an emotional bond is one of the toughest<br> tasks.  And it can be only achieved by understanding the <br>consumer’s day to day journey.</p>
+               <div class="container">
+                  <div class="main-cont">
+                   <div class="row text-center">
+                       <div class="col-md-4">
+                          <div class="twit01 text-left">
+                              <div class="twit-content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                              <hr>
+                              <h4>Venkat Raja</h4>
+                              </div>
+                           </div>
+                       </div>
+                       <div class="col-md-4">
+                            <div class="twit02 text-left">
+                              <div class="twit-content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                              <hr>
+                              <h4>Venkat Raja</h4>
+                              </div>
+                          </div>
+                           
+                       </div>
+                       <div class="col-md-4">
+                            <div class="twit03 text-left">
+                              <div class="twit-content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                              <hr>
+                              <h4>Venkat Raja</h4>
+                              </div>
+                       </div>
+                       </div>
+                   </div>
+                   
+               <div class="row add">
+                   <div class="col-md-3 left-grid"></div>
+                   <div class="col-md-9 right-grid"></div>
+               </div>
+           
+                   
+               </div>
+           </div>
+           
+           
+           
+           </div>
+        
+        
+        
+        
+      
+        </div>
+        
+        <div class="sb-slidebar sb-right">
+    What the fuck
+    </div>
+
+        
+ 
+        
+   
+ <!-- /container -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+</script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+<script src="js/slidebars.min.js"></script>
+<script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
