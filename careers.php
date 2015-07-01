733f636b43a474f262794c376c90577cc3bd34b8
Praveen Raj <praveenraj@uniqpix.in>
2015-07-01 15:05:09 +0530

    careers & News page added

--- careers.php
+++ careers.php
@@ -6,13 +6,22 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Indian Westeros | Careers</title>
+        <title>Careers - Indian Westeros</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php include('link.html'); ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+       
+         <?php include('link.html'); ?>
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
+      <div class="loading">
+    <div class="loading-gif">
+        <img src="img/loading.GIF" alt="Loading">
+    </div>
+</div>   
+    
+           
+       
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -20,75 +29,97 @@
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
         
-        
-        
-        <div class="container-fluid career-index">
-            <?php include('nav.html'); ?>
-        <div class="table"><div class="text-center cell"><div class="intro-text font">CAREERS</div>
-            
-        </div></div>
-             </div>
-        <div class="container-fluid creative-council-wrap text-center">
-        <div class="row ">
-            <div class="col-md-8 col-sm-8" >
-                <h3>Vacancies</h3>
-                <hr style="margin-top: 20px;margin-bottom: 10px;" class="red">
-            <div class="container text-left pd-left">
-                <h3 class="bold">Senior Graphic Designers</h3>
-                <h4>Online Content</h4>
-               <hr class="text-left" style="margin:0 ;"><br>
-                <p>Do you have...</p>
-                <p>&#8226; 3+ years of experience in creating Graphic content</p>
-                 <p>&#8226; experience in web banners and flash</p>
-                 <p>&#8226; impressive skills in image editing and visual effects</p>
-                 <p>&#8226; experience working against timelines</p>
-                 <p>&#8226; expertise in Photoshop, Illustrator, HTML, CSS</p>
-                 <p>&#8226; a B.Sc, VisCom, BCA, MCA, M.Sc degree</p>
-                 <p>&#8226;a positive attitude</p><br>
-                <a href=""><p style="color: #ec1c24">Apply to info@indianwesteros.com</p></a>
+
+    <div id="page" class="push">
+        <div class="container-fluid about-header wow fadeInDown animated">
+        <div class="container text-center">
+            <a href="index.php"><img src="img/logo-about.png" alit="" class="brand-about"/></a>
+        </div>
+<!--<div class="about-menu"><a href="#"><img src="img/menu.png" alt="" class="menu"/></a></div>-->
+        </div>  
+       <div class="container nav-cont text-center wow fadeInDown animated">
+           <ul>
+               <li><a href="about.php">ABOUT</a></li>
+               <li><a href="#">WORK</a></li>
+               <li><a href="#">SERVICES</a></li>
+               <li><a href="#">NEWS &amp; EVENTS</a></li>
+               <li><a href="#">CAREERS</a></li>
+               <li><a href="#contact" >CONTACT</a></li>
+           </ul>
+       </div>
+       <div class="container main-wrap">
+           <div class="big-text01 text-center wow fadeInUp animated">Careers<span style="color: #eb1c24;">.</span></div>
+           </div>
+           
+        <div class="container news-cont">
+            <div class="row news-row">
+                <div class="col-md-4 left-news">
+                </div>
+                <div class="col-md-8 right-news">
+                     <h2>Vaccancies</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-4 council-bg"></div>
+            <div class="row news-row">
+                <div class="col-md-8 right-news">
+                                  <h2>Vaccancies</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                </div>
+                <div class="col-md-4 left-news">
+   
+                </div>
             </div>
-        </div>
-        
-        
-        <div class="container-fluid creative-council-wrap text-center">
-        <div class="row ">
-             <div class="col-md-4 col-sm-4 council-bg"></div>
-            <div class="col-md-8 col-sm-8" >
-                <h3>Vacancies</h3>
-                <hr style="margin-top: 20px;margin-bottom: 10px;" class="red">
-            <div class="container text-left pd-left">
-                <h3 class="bold">Senior Graphic Designers</h3>
-                <h4>Online Content</h4>
-               <hr class="text-left" style="margin:0 ;"><br>
-                <p>Do you have...</p>
-                <p>&#8226; 3+ years of experience in creating Graphic content</p>
-                 <p>&#8226; experience in web banners and flash</p>
-                 <p>&#8226; impressive skills in image editing and visual effects</p>
-                 <p>&#8226; experience working against timelines</p>
-                 <p>&#8226; expertise in Photoshop, Illustrator, HTML, CSS</p>
-                 <p>&#8226; a B.Sc, VisCom, BCA, MCA, M.Sc degree</p>
-                 <p>&#8226; a positive attitude</p><br>
-                <a href=""><p style="color: #ec1c24">Apply to info@indianwesteros.com</p></a>
+            
+             <div class="row news-row">
+                <div class="col-md-4 left-news">
+                </div>
+                <div class="col-md-8 right-news">
+                     <h2>Vaccancies</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                 </div>
             </div>
            
-            </div>
+          
         </div>
-        
-       
-   
-       
-        
+          
+       <div class="container add-main">
+               <div class="row add">
+                   <div class="col-md-3 left-grid"></div>
+                   <div class="col-md-9 right-grid text-left">
+                     
+                       <h3>We’re Hiring!!!</h3>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                       <h4>careers@indianwesteros.com</h4>
+                       
+                   </div>
+               </div></div>
+              
+             
 <?php include('footer.html'); ?>
+   
+<?php include('sidebar.php'); ?>
+
+
+
+ </div>     
+   
+        
    
  <!-- /container --><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="js/vendor/bootstrap.min.js"></script>
-
+        <script src="js/wow.min.js"></script>
+        <script src="js/bigSlide.js"></script>
         <script src="js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
@@ -102,3 +133,5 @@
         </script>
     </body>
 </html>
+
+
--- news.php
+++ news.php
@@ -6,13 +6,22 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Indian Westeros | news</title>
+        <title>News &amp; Events - Indian Westeros</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-       <?php include('link.html'); ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+       
+         <?php include('link.html'); ?>
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
+      <div class="loading">
+    <div class="loading-gif">
+        <img src="img/loading.GIF" alt="Loading">
+    </div>
+</div>   
+    
+           
+       
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -20,99 +29,102 @@
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
         
-        
-        
-        <div class="container-fluid career-index">
-            <?php include('nav.html'); ?>
-        <div class="table"><div class="text-center cell"><div class="intro-text font">NEWS</div>
-            
-        </div></div>
-             </div>
-        <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6 left news01">
-            
-                </div>
-                 <div class="col-md-6 right">
-                     <h4 style="color: #fff;">New launches</h4>
-                     <hr class="text-left red" style="margin:0 ;"><br>
-                     <p style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
-                     <p style="color: #fff;">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     </p>
-                </div>
-                </div>
-            </div>
-             <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6 left news02">
 
-                </div>
-                 <div class="col-md-6 right">
-                     <h4 style="color: #fff;">New launches</h4>
-                     <hr class="text-left red" style="margin:0 ;"><br>
-                     <p style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
-                     <p style="color: #fff;">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     </p>
-                </div>
-                </div>
-            </div>
-        
-            </div>
-            
-            
-             <div class="row">
-            <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6 left news02">
-            
-                </div>
-                 <div class="col-md-6 right">
-                     <h4 style="color: #fff;">New launches</h4>
-                     <hr class="text-left red" style="margin:0 ;"><br>
-                     <p style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
-                     <p style="color: #fff;">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     </p>
-                </div>
-                </div>
-            </div>
-             <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6 left news01">
-
-                </div>
-                 <div class="col-md-6 right">
-                     <h4 style="color: #fff;">New launches</h4>
-                     <hr class="text-left red" style="margin:0 ;"><br>
-                     <p style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
-                     <p style="color: #fff;">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     </p>
-                </div>
-                </div>
-            </div>
-        
-            </div>
-            
+    <div id="page" class="push">
+        <div class="container-fluid about-header wow fadeInDown animated">
+        <div class="container text-center">
+            <a href="index.php"><img src="img/logo-about.png" alit="" class="brand-about"/></a>
         </div>
-        
-        
-       
-       
-   
-       
-        
+<!--<div class="about-menu"><a href="#"><img src="img/menu.png" alt="" class="menu"/></a></div>-->
+        </div>  
+       <div class="container nav-cont text-center wow fadeInDown animated">
+           <ul>
+               <li><a href="about.php">ABOUT</a></li>
+               <li><a href="#">WORK</a></li>
+               <li><a href="#">SERVICES</a></li>
+               <li><a href="#">NEWS &amp; EVENTS</a></li>
+               <li><a href="#">CAREERS</a></li>
+               <li><a href="#contact" >CONTACT</a></li>
+           </ul>
+       </div>
+       <div class="container main-wrap">
+           <div class="big-text01 text-center wow fadeInUp animated">News<span style="color: #eb1c24;">.</span></div>
+           </div>
+           
+        <div class="container news-cont">
+            <div class="row news-row">
+                <div class="col-md-4 left-news">
+                </div>
+                <div class="col-md-8 right-news">
+                     <h2>New Launches</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                </div>
+            </div>
+            <div class="row text-right social-icon">
+               
+                      <ul>
+                      <li class="si3"></li>
+                       <li class="si2"></li>
+                       <li class="si1"></li>
+                       </ul>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-6 small-news">
+                   <div class="smn smn-bg"></div>
+                    <div class="smn-news-cont">
+                         <h2>New Launches</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                    </div>
+                </div>
+                <div class="col-md-6 small-news">
+                    <div class="smn smn-bg"></div>
+                    <div class="smn-news-cont">
+                         <h2>New Launches</h2>
+                     <hr style="border-top: 2px solid #eb1c24; width: 150px; margin-bottom: 1.5em; margin-top: 1.5em;">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+          
+       <div class="container add-main">
+               <div class="row add">
+                   <div class="col-md-3 left-grid"></div>
+                   <div class="col-md-9 right-grid text-left">
+                     
+                       <h3>We’re Hiring!!!</h3>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                       <h4>careers@indianwesteros.com</h4>
+                       
+                   </div>
+               </div></div>
+              
+             
 <?php include('footer.html'); ?>
+   
+<?php include('sidebar.php'); ?>
+
+
+
+ </div>     
+   
+        
    
  <!-- /container --><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="js/vendor/bootstrap.min.js"></script>
-
+        <script src="js/wow.min.js"></script>
+        <script src="js/bigSlide.js"></script>
         <script src="js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
@@ -126,3 +138,5 @@
         </script>
     </body>
 </html>
+
+
